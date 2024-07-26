33196441220009081446853d6e48542774be2f79
DarshanZeus <darshan.mahankar@zeuslearning.com>
2024-07-26 18:38:44 +0530

    stopping Infinite Scrolling

--- SeriosBackend/backendAPI/obj/Debug/net8.0/backendAPI.AssemblyInfo.cs
+++ SeriosBackend/backendAPI/obj/Debug/net8.0/backendAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backendAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f16976302fec300b619a2c464545401d64a469ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b0d1c033bce51df74117824b42995b7b0accfa4")]
 [assembly: System.Reflection.AssemblyProductAttribute("backendAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backendAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
