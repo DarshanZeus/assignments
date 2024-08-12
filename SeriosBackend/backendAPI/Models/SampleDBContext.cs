5660e9aee4a584fa13965aa6fec6c16069bd4790
DarshanZeus <darshan.mahankar@zeuslearning.com>
2024-08-12 19:00:48 +0530

    Backend started

---
+++ SeriosBackend/backendAPI/Models/SampleDBContext.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace backendAPI.Models
+{
+     public partial class SampleDBContext : DbContext
+     {
+       public SampleDBContext(DbContextOptions
+       <SampleDBContext> options)
+           : base(options)
+       {
+       }
+       public virtual DbSet<Customer> Customer { get; set; }
+       protected override void OnModelCreating(ModelBuilder modelBuilder)
+       {
+           modelBuilder.Entity<Customer>(entity => {
+               entity.HasKey(k => k.CustomerId);
+           });
+           OnModelCreatingPartial(modelBuilder);
+       }
+       partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+     }
+}
